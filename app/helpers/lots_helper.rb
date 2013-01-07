6349ca814fa8e95c620b4cc01281df8920769f70
Artur Trofimov <arturtr@gmail.com>
2013-01-08 03:03:29 +1100

    Add lots show page

--- app/helpers/lots_helper.rb
+++ app/helpers/lots_helper.rb
@@ -5,11 +5,11 @@ module LotsHelper
       links = lot.link.split(', ')
       link_tos = []
       names.each_with_index do |name, i|
-        link_tos << link_to(name, links[i], target: '_blank')
+        link_tos << link_to(image_tag("vk20.png", title: name), links[i], target: '_blank')
       end
       raw link_tos.join('<br>')
     else
-      link_to lot.name, lot.link, target: '_blank'
+      link_to image_tag("vk20.png", title: lot.name), lot.link, target: '_blank'
     end
   end
 end
