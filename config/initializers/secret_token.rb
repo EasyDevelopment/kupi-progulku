d474bd8d956ac053e5e2acda9ddcd70c1aebcef8
Artur Trofimov <arturtr@gmail.com>
2013-01-07 20:25:58 +1100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KupiProgulku::Application.config.secret_token = '76ee9cb3abef135d751c99bfde343181ebf1fc374db518a846fbb432d663ea8b3306c6eccfc1899a4cdc9dfb67d2da900dfe8d2d8d19a62ee73444eda9f479df'
