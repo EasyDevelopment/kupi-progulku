6e5ada662692e8cf3792f784a63bb018a5c8df6f
Artur Trofimov <arturtr@gmail.com>
2013-01-08 03:38:55 +1100

    Add arrows

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -1,4 +1,15 @@
 class Lot < ActiveRecord::Base
   belongs_to :stage
   attr_accessible :link, :male, :name, :photo, :photo_big, :pid, :slogan, :stage_id
+  def next
+    lots = Lot.where(male: male, stage_id: stage_id)
+    lot = lots.where("id > ?", id).first
+    lot.present? ? lot : lots.first
+  end
+
+  def prev
+    lots = Lot.where(male: male, stage_id: stage_id)
+    lot = lots.where("id < ?", id).last
+    lot.present? ? lot : lots.last
+  end
 end
