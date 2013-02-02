6095b3be7233700b8c2797ff355c330c33ea8697
Apophis <gsapophis@mail.ru>
2013-02-02 08:55:47 +0200

    bug_fix_simple_auth_reg

--- Gemfile
+++ Gemfile
@@ -36,6 +36,21 @@ group :assets do
 end
 
 gem 'jquery-rails'
+gem "rb-readline"
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# To use Jbuilder templates for JSON
+# gem 'jbuilder'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'debugger'
 
 group :development do
   gem 'annotate'
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,9 @@
 class Users::OmniauthCallbacksController < ApplicationController
   def vkontakte
-  	<hh user=user> = User.find_for_vkontakte_oauth request.env["omniauth.auth"]
-    if <hh user=user>.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Vkontakte"
-      sign_in_and_redirect <hh user=user>, :event => :authentication
+  	user = User.find_for_vkontakte_oauth request.env["omniauth.auth"]
+    if user.persisted?
+      #flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Vkontakte"
+      sign_in_and_redirect user, :event => :authentication
     else
       flash[:notice] = "authentication error"
       redirect_to root_path
