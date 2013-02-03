b9fa5585ef36fd5da5b5dc6211c8731942db39e4
Apophis <gsapophis@mail.ru>
2013-02-03 18:47:23 +0200

    upd_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 KupiProgulku::Application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  resources :users, :only => [:destroy, :edit, :new]
+  resources :users, :only => [:destroy, :edit, :new, :create]]
 
   get "omniauth_callbacks/vkontakte"
 
