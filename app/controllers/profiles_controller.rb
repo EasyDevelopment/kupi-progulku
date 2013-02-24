a3839d886f8b04043d1a2727b7366f975806d8b9
Artur Trofimov <arturtr@gmail.com>
2013-02-24 13:36:15 +1100

    redirect to new profile for new user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,13 @@
 class ProfilesController < InheritedResources::Base
+  def new
+    auth = session[:auth]
 
+    @profile = current_user.build_profile
+
+    @profile.first_name = auth.info.first_name
+    @profile.last_name = auth.info.last_name
+    @profile.photo_url = auth.info.image
+    @profile.nickname = auth.info.name
+    @profile.male = auth.extra.raw_info.sex == 2
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,18 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
+    auth = env['omniauth.auth']
+    user = User.from_omniauth(auth)
     session[:user_id] = user.id
     if user.profile.present?
-      redirect_to root_url, notice: "Signed in!"
+      redirect_to root_url, notice: 'Signed in!'
     else
+      session[:auth] = auth
       redirect_to new_profile_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Signed out!"
+    redirect_to root_url, notice: 'Signed out!'
   end
 end
